4984b0095f5e3c549ef00455488ccc968dcce68b
Luis Eduardo Silva Rodriguez <luis.silva.2708@gmail.com>
2023-12-20 11:55:16 -0500

    03. Typing de TS

---
+++ src/03-typing.ts
@@ -0,0 +1,10 @@
+(()=> {
+  let myProductName: string = 'Product 1';
+  let myProductPrice: number = 123;
+
+  myProductName = 'cambio';
+  myProductPrice.toFixed();
+
+  const myProductStock = 100;
+})();
+
