ba8ecef41d66db90d7f8b4a94b16b275005698ea
Luis Eduardo Silva Rodriguez <luis.silva.2708@gmail.com>
2023-12-20 12:13:26 -0500

    04. Typing de TS - number

---
+++ src/04-numbers.ts
@@ -0,0 +1,36 @@
+(()=> {
+  let productPrice = 100;
+  productPrice = 12;
+  console.log('productPrice: ', productPrice);
+
+  let customerAge: number = 28;
+
+  customerAge = customerAge +1;
+
+  let productInStock: number;
+  console.log('prodcutInStock: ', productInStock);
+  if(productInStock >10){
+    console.log('is greater');
+  }
+
+  let disconunt = parseInt('100');
+  console.log('discount: ', disconunt);
+
+  if(disconunt<=200) {
+    console.log('apply');
+
+  }else {
+    console.log('not apply');
+
+  }
+
+  let hex = 0xfff;
+  console.log(hex);
+  let bin =0b1010;
+  console.log('Binario: ', bin);
+
+  const myNumber: number = 10
+
+
+
+})();
