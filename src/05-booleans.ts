2b4d0b197f20fe243685751dedb3eef2f18c26ae
Luis Eduardo Silva Rodriguez <luis.silva.2708@gmail.com>
2023-12-20 15:53:42 -0500

    05. Typing de TS - boolean

---
+++ src/05-booleans.ts
@@ -0,0 +1,24 @@
+(()=> {
+
+  let isEnable = true;
+
+  isEnable = false;
+
+  let isNew: boolean = false;
+
+  console.log('isNew: ', isNew);
+
+  isNew = true;
+
+  console.log('isNew: ', isNew);
+
+  const random = Math.random();
+
+  console.log('ramdon: ', random);
+
+  isNew = random >= 0.5 ? true : false;
+
+  console.log('isNew: ', isNew);
+
+
+})();
